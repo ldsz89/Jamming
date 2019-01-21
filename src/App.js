6ca27de52017c15ff38683407fe1660f135170ac
BUCKEYE\dshannon <dshannon@theloft.biz>
2019-01-21 15:20:51 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchResults: '',
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Ja<span class="highlight">mmm</span>ing</h1>
+                <div class="App">
+                    <!-- Add a SearchBar component -->
+                    <div class="App-playlist">
+                    <!-- Add a SearchResults component -->
+                    <!-- Add a Playlist component -->
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
