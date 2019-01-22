40cf030003aa0c86d3189076fd6521928e6b529f
Dre Shannon <dshannon@theloft.biz>
2019-01-22 16:23:31 -0600

    Added search template

--- src/App.js
+++ src/App.js
@@ -66,10 +66,12 @@ class App extends Component {
         this.addTrack = this.addTrack.bind(this);
         this.removeTrack = this.removeTrack.bind(this);
         this.updatePlaylistName = this.updatePlaylistName.bind(this);
+        this.savePlaylist = this.savePlaylist.bind(this);
+        this.search = this.search.bind(this);
     }
 
     addTrack(track) {
-        if (this.state.playlistTracks.find((savedTrack) => { return savedTrack.id = track.id })) {
+        if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id )) {
             return;
         }
         let updatedPlaylistTracks = this.state.playlistTracks;
@@ -85,8 +87,18 @@ class App extends Component {
     }
 
     updatePlaylistName(name) {
-        console.log(`Playlist Name: ${name}`);
         this.setState({ playlistName: name });
+    }
+
+    savePlaylist() {
+        let trackURIs = [];
+        this.state.playlistTracks.forEach(track => {
+            trackURIs.push(track.track_uri);
+        });
+    }
+
+    search(query) {
+        console.log(query);
     }
 
     render() {
@@ -94,10 +106,10 @@ class App extends Component {
             <div>
                 <h1>Ja<span className="highlight">mmm</span>ing</h1>
                 <div className="App">
-                    <SearchBar />
+                    <SearchBar onSearch={this.search} />
                     <div className="App-playlist">
                         <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-                        <Playlist name={this.state.playlistName} tracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} />
+                        <Playlist name={this.state.playlistName} tracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
                     </div>
                 </div>
             </div>
